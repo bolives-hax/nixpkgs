76301d266ad5d8de740b5e7d29e3f78b5dcf5f0f
bolives-hax <git@bl0v3.com>
2026-01-29 13:02:05 +0100

    typo

--- nixos/modules/services/torrent/rtorrent.nix
+++ nixos/modules/services/torrent/rtorrent.nix
@@ -191,7 +191,7 @@ in
 
       # XMLRPC
       scgi_local = (cfg.rpcsock)
-      schedule = scgi_group,0,0,"execute.nothrow=chown,\"rtorrentdata\",(cfg.rpcsock)"
+      schedule = scgi_group,0,0,"execute.nothrow=chown,\"torrentdata\",(cfg.rpcsock)"
       schedule = scgi_permission,0,0,"execute.nothrow=chmod,\"g+w,o=\",(cfg.rpcsock)"
 
       network.bind_address.set = "0.0.0.0"
