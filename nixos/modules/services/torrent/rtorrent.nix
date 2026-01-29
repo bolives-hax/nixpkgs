34cf0f3f803815a0c48c999b1e797fed4667ca52
bolives-hax <git@bl0v3.com>
2026-01-29 13:11:19 +0100

    test curlsegfault

--- nixos/modules/services/torrent/rtorrent.nix
+++ nixos/modules/services/torrent/rtorrent.nix
@@ -194,7 +194,7 @@ in
       schedule = scgi_group,0,0,"execute.nothrow=chown,\"torrentdata\",(cfg.rpcsock)"
       schedule = scgi_permission,0,0,"execute.nothrow=chmod,\"g+w,o=\",(cfg.rpcsock)"
 
-      network.bind_address.set = "0.0.0.0"
+      # network.bind_address.set = "0.0.0.0"
       dht.port.set = 5009
     '';
 
