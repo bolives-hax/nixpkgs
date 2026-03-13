f6f0d1041f409242a069de9cf220179a904e1851
bolives-hax <git@bl0v3.com>
2026-03-13 16:58:52 +0100

    reduced rate dirty fix

--- nixos/modules/services/torrent/rtorrent.nix
+++ nixos/modules/services/torrent/rtorrent.nix
@@ -176,6 +176,8 @@ in
       system.cwd.set = (cfg.basedir)
       network.http.dns_cache_timeout.set = 25
       schedule2 = monitor_diskspace, 15, 60, ((close_low_diskspace, 1000M))
+      schedule_remove = session_save
+      schedule2 = session_save , 10 , 86400, ((session.save))
 
       # Watch directories (add more as you like, but use unique schedule names)
       #schedule2 = watch_start, 10, 10, ((load.start, (cat, (cfg.watch), "start/*.torrent")))
